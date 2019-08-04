fcabe674978c52fa556c8144e46835f052f800b4
sriramede9 <only4programming03@gmail.com>
2019-08-04 14:23:47 -0400

    generics a betying type T and can restrict return types and params

--- intro/app.ts
+++ intro/app.ts
@@ -221,6 +221,56 @@ console.log(iphone.getModel());
 
 //enums in typescript
 
-enum daysoftheweek {"s","mon"};
+enum daysoftheweek {
+  "s",
+  "mon"
+}
 
-let today=daysoftheweek.mon;
+let today = daysoftheweek.mon;
+
+//generics
+
+function echo<T>(element: T): T {
+  return element;
+}
+
+echo<number>(2);
+echo<string>("test");
+echo<Array<number>>([1, 2, 3]);
+echo<Array<string>>(["1", "2"]);
+
+class Person3 {
+  constructor(private fname: string, private lname: string) {}
+
+  getDetails() {
+    return `${this.fname} ${this.lname}`;
+  }
+}
+
+class Manager extends Person3 {
+  getDetails() {
+    return ` Manager: ${super.getDetails()}`;
+  }
+}
+
+//now we have few cases to deeply dug through this case
+
+//sol 1
+
+function echoperson(e: Person3): Person3 {
+  return e;
+}
+
+echoperson(new Person3("will", "smith"));
+
+//but the return type is person
+
+//if we want the childern of type person then we can use generics
+
+function echopersonchildren<T extends Person3>(e: T): T {
+  return e;
+}
+
+//now echo perfectly accepts children of Person3 and retur same.
+
+let bw: Manager = echopersonchildren(new Manager("Bruce", "wills"));
