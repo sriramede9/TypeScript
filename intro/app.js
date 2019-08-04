a88e626e3451cea1a57ac516e1aa24bd6dfaeabc
sriramede9 <only4programming03@gmail.com>
2019-08-04 14:01:22 -0400

    classes and interfaces,inheritance,enum and private,protected of constructor

--- intro/app.js
+++ intro/app.js
@@ -1,3 +1,16 @@
+var __extends = (this && this.__extends) || (function () {
+    var extendStatics = function (d, b) {
+        extendStatics = Object.setPrototypeOf ||
+            ({ __proto__: [] } instanceof Array && function (d, b) { d.__proto__ = b; }) ||
+            function (d, b) { for (var p in b) if (b.hasOwnProperty(p)) d[p] = b[p]; };
+        return extendStatics(d, b);
+    };
+    return function (d, b) {
+        extendStatics(d, b);
+        function __() { this.constructor = d; }
+        d.prototype = b === null ? Object.create(b) : (__.prototype = b.prototype, new __());
+    };
+})();
 var Person = { firstname: "Sri", lastname: "ram" };
 //in javascript we can add another unrelated property to the person person.fly="flying";it will add to the object.loosely coupled.
 //we stuck in java script as browser are build to run js.
@@ -55,5 +68,96 @@ function add(a, b, c) {
 add(2, 3);
 //variable capturing quirks.
 for (var i = 0; i < 10; i++) {
-    setTimeout(function () { console.log(i); }, 100 * i);
+    setTimeout(function () {
+        //  console.log(i);
+    }, 100 * i);
 }
+//here output is 10
+for (var i_1 = 0; i_1 < 10; i_1++) {
+    setTimeout(function () {
+        //console.log(i);
+    }, 100 * i_1);
+} //here ouput is 0.1..10
+//we can give options to type as well
+var optational;
+//classes and functions.
+var Person1 = /** @class */ (function () {
+    function Person1(fname, lname) {
+        this.firstname = fname;
+        this.lastname = lname;
+    }
+    Person1.prototype.getFullName = function () {
+        return this.firstname + " " + this.lastname;
+    };
+    return Person1;
+}());
+var aperson = new Person1("a", "b");
+// aperson.firstname = "hero";
+// aperson.lastname = "one";
+console.log(aperson.getFullName());
+//inheritance in typescript
+var Person2 = /** @class */ (function () {
+    function Person2(fname, lname) {
+        this.firstname = fname;
+        this.lastname = lname;
+    }
+    Person2.prototype.greet = function () {
+        return "Hello " + this.firstname + " " + this.lastname;
+    };
+    return Person2;
+}());
+var Customer = /** @class */ (function (_super) {
+    __extends(Customer, _super);
+    function Customer() {
+        return _super !== null && _super.apply(this, arguments) || this;
+    }
+    Customer.prototype.greet = function () {
+        return _super.prototype.greet.call(this) + " how you doing?";
+    };
+    return Customer;
+}(Person2));
+var cust1 = new Customer("k", "l");
+console.log(cust1.greet());
+var Dog = /** @class */ (function () {
+    function Dog() {
+    }
+    Dog.prototype.getdetails = function () {
+        return "This Animal name is " + this.name + " and is of color " + this.color + " and is of age " + this.age;
+    };
+    return Dog;
+}());
+var dog = new Dog(); //polymorphishm subtype supertype
+//ducktyping
+var objduck = {
+    name: "Test",
+    age: 23,
+    color: "blue",
+    getdetails: function () {
+        return "test";
+    },
+    canhaveextraprops: "extra"
+};
+//if swims like a duck and quack likes a duck it is a duck
+dog = objduck;
+//let's tack about geters and setters.
+var Phone = /** @class */ (function () {
+    function Phone() {
+    }
+    Phone.prototype.setModel = function (data) {
+        this.model = data;
+    };
+    Phone.prototype.setImeinum = function (data) {
+        this.imeinum = data;
+    };
+    Phone.prototype.getModel = function () {
+        return this.model;
+    };
+    Phone.prototype.getImeinum = function () {
+        return this.imeinum;
+    };
+    return Phone;
+}());
+var iphone = new Phone();
+iphone.setModel("7");
+iphone.setImeinum("aegh332");
+console.log(iphone.getModel());
--- intro/app.ts
+++ intro/app.ts
@@ -91,13 +91,136 @@ add(2, 3);
 
 for (var i = 0; i < 10; i++) {
   setTimeout(function() {
-    console.log(i);
+    //  console.log(i);
   }, 100 * i);
 }
 //here output is 10
 
 for (let i = 0; i < 10; i++) {
   setTimeout(function() {
-    console.log(i);
+    //console.log(i);
   }, 100 * i);
 } //here ouput is 0.1..10
+
+//we can give options to type as well
+
+let optational: number | string;
+
+//classes and functions.
+
+class Person1 {
+  firstname: string;
+  lastname: string;
+
+  constructor(fname: string, lname: string) {
+    this.firstname = fname;
+    this.lastname = lname;
+  }
+
+  getFullName(): string {
+    return `${this.firstname} ${this.lastname}`;
+  }
+}
+
+var aperson = new Person1("a", "b");
+
+// aperson.firstname = "hero";
+// aperson.lastname = "one";
+
+console.log(aperson.getFullName());
+//inheritance in typescript
+
+class Person2 {
+  firstname: string;
+  lastname: string;
+
+  constructor(fname, lname) {
+    this.firstname = fname;
+    this.lastname = lname;
+  }
+
+  greet() {
+    return `Hello ${this.firstname} ${this.lastname}`;
+  }
+}
+
+class Customer extends Person2 {
+  greet() {
+    return `${super.greet()} how you doing?`;
+  }
+}
+
+let cust1 = new Customer("k", "l");
+console.log(cust1.greet());
+
+//interfaces
+interface Animal {
+  name: string;
+  age: number;
+  color: string;
+  //no methods on interfaces
+  //can define signature
+
+  getdetails(): String;
+}
+
+class Dog implements Animal {
+  name: string;
+  age: number;
+  color: string;
+  getdetails(): String {
+    return `This Animal name is ${this.name} and is of color ${
+      this.color
+    } and is of age ${this.age}`;
+  }
+}
+
+let dog: Animal = new Dog(); //polymorphishm subtype supertype
+
+//ducktyping
+
+let objduck = {
+  name: "Test",
+  age: 23,
+  color: "blue",
+  getdetails: () => {
+    return "test";
+  },
+  canhaveextraprops: "extra"
+};
+
+//if swims like a duck and quack likes a duck it is a duck
+
+dog = objduck;
+
+//let's tack about geters and setters.
+
+class Phone {
+  private model;
+  private imeinum;
+
+  setModel(data: string) {
+    this.model = data;
+  }
+  setImeinum(data: string) {
+    this.imeinum = data;
+  }
+  getModel() {
+    return this.model;
+  }
+  getImeinum() {
+    return this.imeinum;
+  }
+}
+
+let iphone = new Phone();
+iphone.setModel("7");
+iphone.setImeinum("aegh332");
+
+console.log(iphone.getModel());
+
+//enums in typescript
+
+enum daysoftheweek {"s","mon"};
+
+let today=daysoftheweek.mon;
